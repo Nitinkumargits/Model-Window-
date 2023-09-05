4e22c6d519b5d5c7e266f8cb26a849e008648e26
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-05 12:52:15 +0530

    add necessary document querySelector

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Query Selector
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModel);
+
+for (let i = 0; i <= btnsOpenModel.length; i++) {
+  console.log(btnsOpenModel[i].textContent);
+}
