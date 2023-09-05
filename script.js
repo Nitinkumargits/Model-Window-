c28aebdfa945e74ddf38db1e7a706434b5a6a614
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-05 13:53:00 +0530

    Refactoring code -DRY

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModel = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModel);
+// console.log(btnsOpenModel);
+const openModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i <= btnsOpenModel.length; i++) {
-  console.log(btnsOpenModel[i].textContent);
+  btnsOpenModel[i].addEventListener('click', openModal);
+
+  // close Modal
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
